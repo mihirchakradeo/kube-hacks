118323b50c04363ee10e569179c06ae482ad9924
Mihir Chakradeo <mihir.chakradeo@gmail.com>
2019-04-27 17:00:38 -0400

    documentation

--- kdel.sh
+++ kdel.sh
@@ -1,4 +1,9 @@
+#! /bin/bash
+# Use this command to delete pods from kubernetes cli by mathching regex
+# Setup: Copy the following function in your bashrc
+# Usage: kdel <regex pattern>
+# Example: kdel telegraf*
+
 function kdel() {
   k delete $(kg po -o name --no-headers=true | grep $1)
 }
-
